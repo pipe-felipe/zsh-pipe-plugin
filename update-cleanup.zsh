a3882c77ebb5bda0139d0ea1b1fa918cb02776c6
pipe-felipe <alves.n.felipe@gmail.com>
2023-04-19 21:49:44 -0300

    finish update

--- update-cleanup.zsh
+++ update-cleanup.zsh
@@ -1,16 +1,16 @@
 readonly ARCH=/etc/arch-release
 readonly FEDORA=/etc/fedora-release
+readonly UBUNTU=/etc/os-release
+
+# 0 = true | 1 = false
 
 function _os_match() {
   if test -f "$1"; then
-    echo "Found file $1"
     return 0
   else
-    echo "Could not find file $1"
     return 1
   fi
 }
-
 
 function _is_installed {
   if command -v "$1" >/dev/null 2>&1; then
@@ -43,43 +43,27 @@ function _update_flatpak() {
 }
 
 function _os_update() {
-    case 1 in
-  "$(_os_match "$ARCH")")
-      echo "z"
-      ;;
-    "$(_os_match "$FEDORA")")
-      echo "y"
-      ;;
-    *)
-      echo "Unknown OS"
-      ;;
-  esac
-}
-_os_update
+  echo -e "${BLUE}${BOLD}\nSYSTEM UPDATE ${RESET}"
+  printf "\n"
 
-function update() {
-  if _is_arch; then
-    echo -e "${BLUE}${BOLD}\nSYSTEM UPDATE ${RESET}"
-    printf "\n"
+  if test -f $FEDORA; then
+    echo -e "${GREEN}dnf upgrade"
+    sudo dnf upgrade
+  fi
 
+  if test -f $ARCH; then
     echo -e "${GREEN}pacman -Syu"
     sudo pacman -Syu
-    printf "==================================================\n"
-
-    printf "==================================================\n"
-    echo -e "${BOLD}DONE WITH UPDATE"
-  fi
-  if _is_installed snap; then
-    _update_snap
   fi
 
-  if _is_installed brew; then
-    _homebrew_update
+  if [[ -f "$UBUNTU" ]] && grep -qi "ubuntu" "$UBUNTU"; then
+    echo -e "${GREEN}apt update && upgrade"
+    sudo apt update
+    sudo apt upgrade -y
   fi
 
-  if _is_installed flatpak; then
-    _update_flatpak
-  fi
+  printf "==================================================\n"
+  echo -e "${BOLD}DONE WITH UPDATE"
 }
 
 function _cleanup_homebrew() {
@@ -98,24 +82,42 @@ function _cleanup_flatpak() {
   printf "==================================================\n"
 }
 
-function clean() {
-  if _is_arch; then
-    echo -e "${BLUE}${BOLD}\nSYSTEM CLEANUP ${RESET}"
-    printf "\n"
+function _os_clean() {
+  echo -e "${BLUE}${BOLD}\nSYSTEM CLEANUP ${RESET}"
+  printf "\n"
 
+  if test -f $FEDORA; then
+    echo -e "${GREEN}dnf clean all"
+    sudo dnf clean all
+  fi
+
+  if test -f $ARCH; then
     echo -e "${GREEN}-Rsn pacman -Qtdq"
     sudo pacman -Rns "$(pacman -Qtdq)"
-    printf "==================================================\n"
-
-    if _is_installed brew; then
-      _cleanup_homebrew
-    fi
-
-    if _is_installed flatpak; then
-      _cleanup_flatpak
-    fi
-
-    printf "==================================================\n"
-    echo -e "${BOLD}DONE WITH CLEANUP"
   fi
+
+  if [[ -f "$UBUNTU" ]] && grep -qi "ubuntu" "$UBUNTU"; then
+    echo -e "${GREEN}apt clean autoclean autoremove"
+    sudo apt autoclean
+    sudo apt clean
+    sudo apt autoremove -y
+  fi
+
+  printf "==================================================\n"
+  echo -e "${BOLD}DONE WITH CLEANUP"
+}
+
+function update() {
+  _os_update
+
+  if _is_installed snap; then _update_snap; fi
+  if _is_installed brew; then _homebrew_update; fi
+  if _is_installed flatpak; then _update_flatpak; fi
+}
+
+function clean() {
+  _os_clean
+
+  if _is_installed brew; then _cleanup_homebrew; fi
+  if _is_installed flatpak; then _cleanup_flatpak; fi
 }
