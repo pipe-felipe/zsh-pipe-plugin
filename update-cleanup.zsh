6aafefb8c97b36bdcd90d70641a5a88be8109c1b
pipe-felipe <alves.n.felipe@gmail.com>
2023-06-01 21:29:17 -0300

    Add KDE Neon support

--- update-cleanup.zsh
+++ update-cleanup.zsh
@@ -2,133 +2,144 @@ readonly OS_ARCH=/etc/arch-release
 readonly OS_FEDORA=/etc/fedora-release
 readonly OS_UBUNTU=/etc/os-release
 readonly OS_SUSE=/usr/etc/SUSE-brand
+readonly OS_NEON=/etc/os-release
 
 # 0 = true | 1 = false
 
 function _os_match() {
-  if test -f "$1"; then
-    return 0
-  else
-    return 1
-  fi
+	if test -f "$1"; then
+		return 0
+	else
+		return 1
+	fi
 }
 
 function _is_installed {
-  if command -v "$1" >/dev/null 2>&1; then
-    return 0
-  else
-    return 1
-  fi
+	if command -v "$1" >/dev/null 2>&1; then
+		return 0
+	else
+		return 1
+	fi
 }
 
 function _homebrew_update() {
-  echo -e "${YELLOW}brew update"
-  brew update
-  printf "==================================================\n"
+	echo -e "${YELLOW}brew update"
+	brew update
+	printf "==================================================\n"
 
-  echo -e "${YELLOW}brew upgrade"
-  brew upgrade
-  printf "\n"
+	echo -e "${YELLOW}brew upgrade"
+	brew upgrade
+	printf "\n"
 }
 
 function _update_snap() {
-  echo -e "${YELLOW}snap refresh"
-  sudo snap refresh
-  printf "\n"
+	echo -e "${YELLOW}snap refresh"
+	sudo snap refresh
+	printf "\n"
 }
 
 function _update_flatpak() {
-  echo -e "${YELLOW}flatpak update"
-  flatpak update
-  printf "\n"
+	echo -e "${YELLOW}flatpak update"
+	flatpak update
+	printf "\n"
 }
 
 function _os_update() {
-  if test -f "$OS_FEDORA"; then
-    echo -e "${GREEN}dnf upgrade"
-    sudo dnf upgrade
-  fi
+	if test -f "$OS_FEDORA"; then
+		echo -e "${GREEN}dnf upgrade"
+		sudo dnf upgrade
+	fi
 
-  if test -f "$OS_ARCH"; then
-    echo -e "${GREEN}pacman -Syu"
-    sudo pacman -Syu
-  fi
+	if test -f "$OS_ARCH"; then
+		echo -e "${GREEN}pacman -Syu"
+		sudo pacman -Syu
+	fi
 
-  if [[ -f "$OS_UBUNTU" ]] && grep -qi "ubuntu" "$OS_UBUNTU"; then
-    echo -e "${GREEN}apt update && upgrade"
-    sudo apt update
-    sudo apt upgrade -y
-  fi
+	if [[ -f "$OS_UBUNTU" ]] && grep -qi "ubuntu" "$OS_UBUNTU"; then
+		echo -e "${GREEN}apt update && upgrade"
+		sudo apt update
+		sudo apt upgrade -y
+	fi
 
-  if test -f "$OS_SUSE"; then
-    echo -e "${GREEN}zypper up"
-    sudo zypper up
-  fi
+	if test -f "$OS_SUSE"; then
+		echo -e "${GREEN}zypper up"
+		sudo zypper up
+	fi
+
+	if [[ -f "$OS_NEON" ]] && grep -qi "ubuntu" "$OS_NEON"; then
+		echo -e "${GREEN}pkcon refresh and update"
+		sudo pkcon refresh
+		sudo pkcon update
+	fi
 }
 
 function _cleanup_homebrew() {
-  echo -e "${YELLOW}brew cleanup"
-  brew cleanup
-  printf "==================================================\n"
+	echo -e "${YELLOW}brew cleanup"
+	brew cleanup
+	printf "==================================================\n"
 
-  echo -e "${YELLOW}brew autoremove"
-  brew autoremove
-  printf "\n"
+	echo -e "${YELLOW}brew autoremove"
+	brew autoremove
+	printf "\n"
 }
 
 function _cleanup_flatpak() {
-  echo -e "${YELLOW}flatpak --unused"
-  flatpak uninstall --unused
-  printf "\n"
+	echo -e "${YELLOW}flatpak --unused"
+	flatpak uninstall --unused
+	printf "\n"
 }
 
 function _os_clean() {
-  if test -f "$OS_FEDORA"; then
-    echo -e "${GREEN}dnf clean all"
-    sudo dnf clean all
-  fi
+	if test -f "$OS_FEDORA"; then
+		echo -e "${GREEN}dnf clean all"
+		sudo dnf clean all
+	fi
 
-  if test -f "$OS_ARCH"; then
-    echo -e "${GREEN}-Rsn pacman -Qtdq"
-    sudo pacman -Rns "$(pacman -Qtdq)"
-  fi
+	if test -f "$OS_ARCH"; then
+		echo -e "${GREEN}-Rsn pacman -Qtdq"
+		sudo pacman -Rns "$(pacman -Qtdq)"
+	fi
 
-  if [[ -f "$OS_UBUNTU" ]] && grep -qi "ubuntu" "$OS_UBUNTU"; then
-    echo -e "${GREEN}apt clean autoclean autoremove"
-    sudo apt autoclean
-    sudo apt clean
-    sudo apt autoremove -y
-  fi
+	if [[ -f "$OS_UBUNTU" || -f "$OS_NEON" ]]; then
+		if grep -qi "ubuntu" "$OS_UBUNTU" || grep -qi "neon" "$OS_NEON"; then
+			echo -e "${GREEN}apt clean autoclean autoremove"
+			sudo apt autoclean
+			sudo apt clean
+			sudo apt autoremove -y
+		fi
+	fi
 
-  if test -f "$OS_SUSE"; then
-    echo -e "${GREEN}zypper up"
-    sudo zypper clean
-  fi
+	if test -f "$OS_SUSE"; then
+		echo -e "${GREEN}zypper up"
+		sudo zypper clean
+	fi
 }
 
+_os_clean
+
 function update() {
-  echo -e "${BLUE}${BOLD}\nSYSTEM UPDATE ${RESET}"
-  printf "\n"
+	echo -e "${BLUE}${BOLD}\nSYSTEM UPDATE ${RESET}"
+	printf "\n"
 
-  _os_update
+	_os_update
 
-  if _is_installed snap; then _update_snap; fi
-  if _is_installed brew; then _homebrew_update; fi
-  if _is_installed flatpak; then _update_flatpak; fi
+	if _is_installed snap; then _update_snap; fi
+	if _is_installed brew; then _homebrew_update; fi
+	if _is_installed flatpak; then _update_flatpak; fi
 
-  printf "==================================================\n"
-  echo -e "${BOLD}DONE WITH UPDATE"
+	printf "==================================================\n"
+	echo -e "${BOLD}DONE WITH UPDATE"
 }
 
 function clean() {
-  echo -e "${BLUE}${BOLD}\nSYSTEM CLEANUP ${RESET}"
-  printf "\n"
+	echo -e "${BLUE}${BOLD}\nSYSTEM CLEANUP ${RESET}"
+	printf "\n"
 
-  _os_clean
+	_os_clean
 
-  if _is_installed brew; then _cleanup_homebrew; fi
-  if _is_installed flatpak; then _cleanup_flatpak; fi
+	if _is_installed brew; then _cleanup_homebrew; fi
+	if _is_installed flatpak; then _cleanup_flatpak; fi
 
-  printf "==================================================\n"
-  echo -e "${BOLD}DONE WITH CLEANUP"
+	printf "==================================================\n"
+	echo -e "${BOLD}DONE WITH CLEANUP"
 }
