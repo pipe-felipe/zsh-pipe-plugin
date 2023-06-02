5f8f75c064a5353619c490399c53d46a33acba71
Felipe Alves <104647149+pipe-felipe@users.noreply.github.com>
2023-06-01 21:30:35 -0300

    Add KDE Neon support (#4)

--- variables.zsh
+++ variables.zsh
@@ -2,3 +2,4 @@ export readonly OS_ARCH=/etc/arch-release
 export readonly OS_FEDORA=/etc/fedora-release
 export readonly OS_UBUNTU=/etc/os-release
 export readonly OS_SUSE=/usr/etc/SUSE-brand
+export readonly OS_NEON=/etc/os-release
